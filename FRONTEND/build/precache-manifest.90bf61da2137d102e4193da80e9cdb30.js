864968ce125bb2ccf729ec7fd07cb20941efeb02
phamm25 <122081592+phamm25@users.noreply.github.com>
2023-01-09 10:28:23 -0500

    Add files via upload

---
+++ FRONTEND/build/precache-manifest.90bf61da2137d102e4193da80e9cdb30.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1d0a22aca50e51606079469a6be9a4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a86e6b3b9ee7a7b860b3",
+    "url": "/static/css/0.f3c4efa8.chunk.css"
+  },
+  {
+    "revision": "35af54e2895667c3f94b",
+    "url": "/static/css/4.f15730c6.chunk.css"
+  },
+  {
+    "revision": "98c9164748476fdae7e7",
+    "url": "/static/css/5.0c64e6ff.chunk.css"
+  },
+  {
+    "revision": "aae30dc240290d0a03f8",
+    "url": "/static/css/6.630e0698.chunk.css"
+  },
+  {
+    "revision": "aaa9e1f88a018cae7c52",
+    "url": "/static/css/7.98164201.chunk.css"
+  },
+  {
+    "revision": "ab582fcb7980ca324a67",
+    "url": "/static/css/8.a081615d.chunk.css"
+  },
+  {
+    "revision": "8291f86ccacd220deb62",
+    "url": "/static/css/main.bce5cd60.chunk.css"
+  },
+  {
+    "revision": "a86e6b3b9ee7a7b860b3",
+    "url": "/static/js/0.c694e87b.chunk.js"
+  },
+  {
+    "revision": "e4d4f07764fc181b6483",
+    "url": "/static/js/3.c635fd3d.chunk.js"
+  },
+  {
+    "revision": "35af54e2895667c3f94b",
+    "url": "/static/js/4.d15f9c5b.chunk.js"
+  },
+  {
+    "revision": "98c9164748476fdae7e7",
+    "url": "/static/js/5.e5c88fb8.chunk.js"
+  },
+  {
+    "revision": "aae30dc240290d0a03f8",
+    "url": "/static/js/6.0eafc76f.chunk.js"
+  },
+  {
+    "revision": "aaa9e1f88a018cae7c52",
+    "url": "/static/js/7.d182819f.chunk.js"
+  },
+  {
+    "revision": "ab582fcb7980ca324a67",
+    "url": "/static/js/8.dabb0407.chunk.js"
+  },
+  {
+    "revision": "8291f86ccacd220deb62",
+    "url": "/static/js/main.0ab7867f.chunk.js"
+  },
+  {
+    "revision": "5f849656f6fd526030f8",
+    "url": "/static/js/runtime-main.3b2b316d.js"
+  }
+]);
